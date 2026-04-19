9486935ee3d45ecec13cc3541333add8420f6b0c
Ibrahim Danisman <121925671+AEPdestiny@users.noreply.github.com>
2026-04-19 21:45:12 +0200

    spring ordner hinzugefügt

---
+++ Kleidungslager/src/test/java/de/htw_berlin/Kleidungslager/KleidungslagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.Kleidungslager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KleidungslagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
