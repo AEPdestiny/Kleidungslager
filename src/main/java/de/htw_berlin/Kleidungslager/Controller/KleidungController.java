ad4329f8eb827f660d4347cbd5d890a494b324a6
Ibrahim Danisman <121925671+AEPdestiny@users.noreply.github.com>
2026-05-25 03:55:12 +0200

    delete funktion

--- src/main/java/de/htw_berlin/Kleidungslager/Controller/KleidungController.java
+++ src/main/java/de/htw_berlin/Kleidungslager/Controller/KleidungController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import de.htw_berlin.Kleidungslager.Repository.KleidungRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @RestController
@@ -29,5 +31,12 @@ public class KleidungController {
             @RequestBody Kleidungsstuecke kleidungsstueck
     ) {
         return kleidungRepository.save(kleidungsstueck);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteKleidung(
+            @PathVariable Long id
+    ) {
+        kleidungRepository.deleteById(id);
     }
 }
